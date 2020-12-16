635777ff437951b09cd23a99c56384f3061ec7f4
melvin-viana <mjviana057@gmail.com>
2020-12-15 18:32:22 -0800

    Day6

--- day6.js
+++ day6.js
@@ -14,28 +14,53 @@ const checkGroups = () => {
   return counter;
 };
 
-// Function that checks one group
-// Parameters = index
+//! Part One
+// const checkCurrentGroup = (index) => {
+//   const alphabets = {};
+//   let counter = 0;
+//   let currentIndex = index;
+//   // Loop through indices until the current line is empty or undefined
+//   while (input[currentIndex] !== '' && input[currentIndex] !== undefined) {
+//     const currentLine = input[currentIndex];
+//     for (let i = 0; i < currentLine.length; i++) {
+//       // Use current character's code number and subtract by 97
+//       let alphabet = currentLine[i]
+//       // Set the val to true with that value if false
+//       if(!alphabets[alphabet]) {
+//         counter++;
+//         alphabets[alphabet] = true;
+//       }
+//     }
+//     currentIndex++;
+//   }
+//   currentIndex++;
+//   return [counter, currentIndex];
+// };
+
+//! Part Two
 const checkCurrentGroup = (index) => {
   const alphabets = {};
   let counter = 0;
   let currentIndex = index;
   // Loop through indices until the current line is empty or undefined
+  let numOfMembers = 0;
   while (input[currentIndex] !== '' && input[currentIndex] !== undefined) {
     const currentLine = input[currentIndex];
     for (let i = 0; i < currentLine.length; i++) {
       // Use current character's code number and subtract by 97
       let alphabet = currentLine[i]
       // Set the val to true with that value if false
-      if(!alphabets[alphabet]) {
-        counter++;
-        alphabets[alphabet] = true;
-      }
+      alphabets[alphabet] =  alphabets[alphabet] ?  alphabets[alphabet] + 1 :  1;
     }
     currentIndex++;
+    numOfMembers++;
+  }
+  for (let key in alphabets) {
+    if(alphabets[key] === numOfMembers) counter++;
   }
   currentIndex++;
   return [counter, currentIndex];
 };
+
 
 console.log(checkGroups());
